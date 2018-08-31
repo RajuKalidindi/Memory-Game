8f3c87047414db60845008b5cb3355add7a3c24c
RajuKalidindi <krkraju997@gmail.com>
2018-08-31 23:34:39 +0530

    Updated JS

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const cards = document.querySelectorAll('.memory-card');
 
 function flipCard() {
   this.classList.toggle('flip');
+
+  if(!flipped) {
+    flipped = true;
+    card1 = this;
+  }
+  else {
+    flipped = false;
+    card2 = this;
+  }
 }
+
+var flipped = false;
+var card1,card2;
 
 cards.forEach(card => card.addEventListener('click', flipCard));
