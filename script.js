535829908975c0695249b4719bb64e3ede692d99
RajuKalidindi <krkraju997@gmail.com>
2018-09-01 02:25:19 +0530

    Matching completed

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ function flipCard() {
   else {
     flipped = false;
     card2 = this;
+
+    if(card1.dataset.name === card2.dataset.name) {
+      card1.removeEventListener('click', flipCard);
+      card2.removeEventListener('click', flipCard);
+    }
+    else {
+      setTimeout(() => {
+        card1.classList.remove('flip');
+        card2.classList.remove('flip');
+      }, 1300);
+    }
   }
 }
 
