17f2fe4e9413f1bd53febb1769cffa25a8f03c21
RajuKalidindi <krkraju997@gmail.com>
2018-09-12 22:12:50 +0530

    Added 3x4

---
+++ script1.js
@@ -0,0 +1,64 @@
+const cards = document.querySelectorAll('.memory-card');
+
+function flipCard() {
+  if(lock) return;
+  if(this === card1) return;
+
+  this.classList.toggle('flip');
+
+  if(!flipped) {
+    flipped = true;
+    card1 = this;
+  }
+  else {
+    card2 = this;
+
+    if(card1.dataset.name === card2.dataset.name) {
+      card1.removeEventListener('click', flipCard);
+      card2.removeEventListener('click', flipCard);
+      count++;
+      if(count == 6)gameWon();
+
+        reset();
+    }
+    else {
+      lock = true;
+      setTimeout(() => {
+        card1.classList.remove('flip');
+        card2.classList.remove('flip');
+
+        reset();
+      }, 1300);
+    }
+    moves++;
+    document.getElementById('moves').innerHTML = moves;
+  }
+}
+
+function reset() {
+  flipped = false;
+  lock = false;
+  card1 = null;
+  card2 = null;
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    var random = Math.floor(Math.random() * 12);
+    card.style.order = random;
+  });
+})()
+
+function gameWon() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
+var flipped = false;
+var card1,card2;
+var lock = false;
+var count = 0;
+var moves = 0;
+var best = 100;
+
+cards.forEach(card => card.addEventListener('click', flipCard));
